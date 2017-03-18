b70d94f79aa1f82344bf9305fde4a8df41983a88
agnenevulyte <you@example.com>
2017-03-18 13:48:05 +0000

    second commit

--- exercises.js
+++ exercises.js
@@ -39,12 +39,23 @@ function sumArray(numbers, cb) {
 function forEach(arr, cb) {
 	//iterate over arr and pass its values to cb one by one
 	//hint: you will be invoking cb multiple times (once for each value in the array)
+	arr.forEach(function(num) {
+		cb(num);
+	});
 }
 
 function map(arr, cb) {
 	//create a new array
 	//iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
 	//the new array should be the same length as the array argument
+	var newArr = [];
+	arr.map(function(num) {
+		var val = cb(num);
+		newArr.push(val);
+	});
+	return newArr;
+
+	
 }
 
 function getUserConstructor() {
